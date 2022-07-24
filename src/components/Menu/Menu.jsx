a598751f81a312e23ead14bc691133b8b09012ac
Eduard Constantin <ibinceanu.eduard@yahoo.com>
2022-07-24 19:54:46 +0300

    Refactor menu bar

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,11 +1,42 @@
 import React, { useState } from 'react';
 import MenuBar from '../MenuBar/MenuBar';
 import { motion } from 'framer-motion';
-import { BsList } from 'react-icons/bs';
+import { BsList, BsFillBootstrapFill } from 'react-icons/bs';
 import style from './menu.module.css';
 
 export default function Menu() {
 	const [isOpen, setIsOpen] = useState(false);
+
+	const menuElements = [
+		{
+			buttonIcon: <BsFillBootstrapFill />,
+			buttonLabel: 'test',
+			onClick: () => {
+				console.log('test');
+			},
+		},
+		{
+			buttonIcon: <BsFillBootstrapFill />,
+			buttonLabel: 'test1',
+			onClick: () => {
+				console.log('test1');
+			},
+		},
+		{
+			buttonIcon: <BsFillBootstrapFill />,
+			buttonLabel: 'test2',
+			onClick: () => {
+				console.log('test2');
+			},
+		},
+		{
+			buttonIcon: <BsFillBootstrapFill />,
+			buttonLabel: 'test3',
+			onClick: () => {
+				console.log('test3');
+			},
+		},
+	];
 
 	return (
 		<motion.div className={style.menuContainer}>
@@ -18,7 +49,7 @@ export default function Menu() {
 			>
 				<BsList />
 			</motion.button>
-			<MenuBar isOpen={isOpen} />
+			<MenuBar isOpen={isOpen} menuElements={menuElements} />
 		</motion.div>
 	);
 }
--- src/components/MenuBar/MenuBar.jsx
+++ src/components/MenuBar/MenuBar.jsx
@@ -4,7 +4,7 @@ import { motion, useTransform, useMotionValue } from 'framer-motion';
 import { menuAnim } from './Menu.anim';
 import style from './MenuBar.module.css';
 
-export default function MenuBar({ isOpen = 'closed', angle = 5 }) {
+export default function MenuBar({ menuElements, isOpen, angle }) {
 	const x = useMotionValue(0);
 	const rotateY = useTransform(x, [0, 500], [-angle, angle]);
 	function handleMouse(event) {
@@ -22,19 +22,47 @@ export default function MenuBar({ isOpen = 'closed', angle = 5 }) {
 			style={{ rotateY: rotateY }}
 		>
 			<ul className={`${style.menuList} ${style.long}`}>
-				<li>
-					<MenuLink />
-				</li>
-				<li>
-					<MenuLink />
-				</li>
-				<li>
-					<MenuLink />
-				</li>
-				<li>
-					<MenuLink />
-				</li>
+				{menuElements.map((el) => {
+					return (
+						<li key={el.buttonLabel}>
+							<MenuLink icon={el.buttonIcon} buttonLabel={el.buttonLabel} onClick={el.onClick} />
+						</li>
+					);
+				})}
 			</ul>
 		</motion.div>
 	);
 }
+
+const defaultElements = [
+	{
+		buttonLabel: 'element1',
+		onClick: () => {
+			console.log('element1');
+		},
+	},
+	{
+		buttonLabel: 'element2',
+		onClick: () => {
+			console.log('element2');
+		},
+	},
+	{
+		buttonLabel: 'element3',
+		onClick: () => {
+			console.log('element3');
+		},
+	},
+	{
+		buttonLabel: 'element4',
+		onClick: () => {
+			console.log('element4');
+		},
+	},
+];
+
+MenuBar.defaultProps = {
+	isOpen: 'closed',
+	angle: 5,
+	menuElements: defaultElements,
+};
